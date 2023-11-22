96f77ecda0dea924f81dfd898c694b486edc454f
mono <traineralaina@gmail.com>
2023-11-22 12:02:57 -0700

    updated models again

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -21,12 +21,18 @@ Product.init(
     },
     price: {
       type: DataTypes.DECIMAL(10, 2),
-      allowNull: false
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
     },
     stock: {
       type: DataTypes.INTEGER,
-      allowNull: true,
-      defaultValue: 0
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isInteger: true
+      }
     },
     category_id: {
       type: DataTypes.INTEGER,
